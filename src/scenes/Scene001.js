c205efb4d6a5a7904208a925c27bf7ab0b51d956
David Segura Payá <david.segura@richaudience.com>
2022-01-12 17:55:13 +0100

    Add - class 010

--- src/scenes/Scene001.js
+++ src/scenes/Scene001.js
@@ -1,4 +1,5 @@
-import { Scene, Color, DirectionalLight, HemisphereLight, Group } from 'three';
+import { CameraHelper } from 'three';
+import { Scene, Color, DirectionalLight, HemisphereLight, Group, Camera } from 'three';
 import { Cube } from '../objects/Cube';
 class Scene1 extends Scene {
 	constructor() {
@@ -10,10 +11,12 @@ class Scene1 extends Scene {
 	create() {
 		this.brick = new Cube(2, new Color('orangered').convertSRGBToLinear());
         this.brick.position.set(0, 0, 0);
+        this.brick.castShadow = true;
 		//this.add(this.brick);
 
         this.floor = new Cube(7, new Color('rgb(255,255,255)').convertSRGBToLinear());
         this.floor.position.set(0, -4.5, 0);
+        this.floor.receiveShadow = true;
 
         this.add(this.brick, this.floor);
         //this.group = new Group();
@@ -23,7 +26,19 @@ class Scene1 extends Scene {
 
 		const ambientLight = new HemisphereLight(0xffffbb, 0x080820, .5);
 		const light = new DirectionalLight(0xffffff, 1.0);
-		this.add(light, ambientLight);
+        light.position.set(1, 1, 0)
+        light.castShadow = true;
+
+        /*light.shadow.camera.top = 1;
+        light.shadow.camera.bottom = -1;
+        light.shadow.camera.left = -1;
+        light.shadow.camera.right = 1;*/
+
+        light.shadow.camera.far = 4;
+
+        const helper = new CameraHelper(light.shadow.camera);
+
+		this.add(light, ambientLight, helper);
 	}
 
 	update() {
