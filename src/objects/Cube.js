26f49c2c1179b75e9c3b0da5f19f7dedac904102
David Segura Payá <david.segura@richaudience.com>
2022-01-12 17:37:35 +0100

    Updated Scene - class 009

--- src/objects/Cube.js
+++ src/objects/Cube.js
@@ -1,12 +1,12 @@
 import { Mesh, MeshStandardMaterial, Color, BoxBufferGeometry } from 'three';
 
 export class Cube extends Mesh {
-	constructor(size) {
+	constructor(size, color) {
 		super();
 
 		this.geometry = new BoxBufferGeometry(size, size, size);
 		this.material = new MeshStandardMaterial({
-			color: new Color('orangered').convertSRGBToLinear(),
+			color: color,
 			flatShading: true,
 			roughness: .5
 		});
